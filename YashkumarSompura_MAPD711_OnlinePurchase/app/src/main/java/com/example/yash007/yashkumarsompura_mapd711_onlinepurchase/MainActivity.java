a0e4dae819af9b5adedb44b8fb25066a78c48891
YASH SOMPURA <yash007@YASHs-MacBook-Pro.local>
2017-12-14 15:39:34 -0500

    Assignment Created

---
+++ YashkumarSompura_MAPD711_OnlinePurchase/app/src/main/java/com/example/yash007/yashkumarsompura_mapd711_onlinepurchase/MainActivity.java
@@ -0,0 +1,19 @@
+/*
+    Project Name: Online Purchase System for Assignment - 3
+    Developed By: Yashkumar Sompura
+                  Shivam Shah
+ */
+
+package com.example.yash007.yashkumarsompura_mapd711_onlinepurchase;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
